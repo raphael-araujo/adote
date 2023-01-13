fbb9cc0daf0a39f6bf67f86e12d75422e83bee95
Raphael Araújo <raphaelaraujotoc@outlook.com>
2023-01-13 17:56:01 -0300

    criando tabelas para o cadastro de pets

--- divulgar/admin.py
+++ divulgar/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import Pet, Raca, Tag
+
 # Register your models here.
+
+admin.site.register(Raca)
+admin.site.register(Tag)
+
+@admin.register(Pet)
+class PetAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'raca')
+    prepopulated_fields = {'slug': ('nome',)}
