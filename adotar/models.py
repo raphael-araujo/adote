13081f2559fcbd312f693edc293147eeb2f3191f
Raphael Araújo <raphaelaraujotoc@outlook.com>
2023-01-16 20:20:56 -0300

    criando tabela para pedidos de adoções

--- adotar/models.py
+++ adotar/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+from divulgar.models import Pet
+
 # Create your models here.
+
+
+class PedidoAdocao(models.Model):
+    choices_status = (
+        ('AG', 'Aguardando Aprovação'),
+        ('AP', 'Aprovado'),
+        ('R', 'Recusado')
+    )
+
+    pet = models.ForeignKey(Pet, on_delete=models.DO_NOTHING)
+    usuario = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    data = models.DateTimeField()
+    status = models.CharField(max_length=2, choices=choices_status, default='AG')
+    
+    def __str__(self):
+        return f'{self.usuario} - Pet {self.pet}'
+
+    class Meta:
+        verbose_name_plural = 'Pedidos de Adoções'
